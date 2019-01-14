04363ae52a96b3e461e3ef7ad7a22332d84fab66
Adam Blair <apb@linuxmail.org>
2019-01-13 17:58:35 -0700

    Init Commit

---
+++ src/main/java/com/omni/omni/OmniApplication.java
@@ -0,0 +1,14 @@
+package com.omni.omni;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OmniApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OmniApplication.class, args);
+    }
+
+}
+
